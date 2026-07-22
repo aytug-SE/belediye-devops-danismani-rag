afa67a4d2a2c9b155de034408a0c0cc37748686d
aytug-SE <miladerimranked@gmail.com>
2026-07-22 16:27:28 +0300

    Update print statements to English

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,10 +1,10 @@
 from datasets import load_dataset
 
 def get_prepared_dataset(file_path):
-    print("Veri seti yükleniyor...")
+    print("Dataset is loading...")
     # Load the dataset from the specified file path using the Hugging Face 'load_dataset' function
     dataset = load_dataset("json", data_files=file_path, split="train")
-    print(f"Veri seti başarıyla yüklendi. Toplam satır sayısı: {len(dataset)}")
+    print(f"Dataset successfully loaded. Total number of rows.: {len(dataset)}")
     return dataset
 
 def formatting_prompts_func(example):
@@ -15,7 +15,7 @@ def formatting_prompts_func(example):
     # TinyLlama için ideal chat şablonu (Instruction ve Input ayrılmış durumda)
     text = (
         f"<|system|>\n"
-        f"You are a helpful DevOps assistant.\n"
+        f"You are a helpful DevOps Assistant\n"
         f"<|user|>\n"
         f"{instruction}\n\nLog/System Context:\n{user_input}\n"
         f"<|assistant|>\n"
